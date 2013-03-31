1923742f5a0afdb38080f86d6e62842f3894d8e1
Patrick McLenithan <patrick.mclenithan@gmail.com>
2013-03-30 17:26:36 -0700

    update UserSchema

--- model/schemas/UserSchema.js
+++ model/schemas/UserSchema.js
@@ -3,13 +3,23 @@ var mongoose = require('mongoose')
 
 var UserSchema = new Schema({
   username: String,
-  fName: String,
-  lName: String,
+  name: {
+  	first: String,
+  	last: String
+  },
   password: String,
+  email: String,
   roleId: Number,
   createdAt: { type: Date, default: Date.now },
   deletedAt: Date
-}, { autoIndex: false });
+}, { autoIndex: false, collection: 'Users' });
+
+UserSchema.virtual('name.full').set(function (name) {
+  var split = name.split(' ');
+  this.name.first = split[0];
+  this.name.last = split[1];
+});
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {
