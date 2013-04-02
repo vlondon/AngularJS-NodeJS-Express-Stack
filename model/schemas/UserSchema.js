1547fb057b51f786652af52f9d11a582811a283a
Patrick McLenithan <patrick.mclenithan@gmail.com>
2013-04-02 00:36:17 -0700

    update users

--- model/schemas/UserSchema.js
+++ model/schemas/UserSchema.js
@@ -2,24 +2,18 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  username: String,
+  username: { type: String, lowercase: true, required: true, unique: true },
   name: {
-  	first: String,
-  	last: String
+  	first: { type: String, required: true },
+  	last: { type: String, required: true }
   },
-  password: String,
-  email: String,
+  password: { type: String, required: true },
+  email: { type: String, unique: true, required:true },
   roleId: { type: Number, default: 5 },
   lastLogin: { type: Date , default: Date.now },
   createdAt: { type: Date, default: Date.now },
   deletedAt: { type: Date, default: null }
 }, { autoIndex: false, collection: 'Users' });
-
-UserSchema.virtual('name.full').set(function (name) {
-  var split = name.split(' ');
-  this.name.first = split[0];
-  this.name.last = split[1];
-});
 
 var User = mongoose.model('User', UserSchema);
 
--- resources/users.js
+++ resources/users.js
@@ -2,15 +2,31 @@ var   Model = EpiManager.Model
 	, _ = require("underscore")
 	, async = require('async');
 
+var validFields =  ['username',
+					'name',
+					'password',
+					'email',
+					'roleId'];
+var showFields =   ['username',
+					'_id',
+					'name',
+					'email',
+					'lastLogin',
+					'roleId',
+					'createdAt'];
+
 exports.index = function(req, res) {
-	console.log('pinged');
-	Model.User.find({}, function (err, person) {
+	Model.User.find({
+		deletedAt: null
+	}, function (err, person) {
 		if (err) return console.log(err);
-		res.send(201, person);
+		res.send(201, _.pick(person, showFields));
 	});
 };
 exports.create = function(req, res) {
-	var data = req.body;
+	// Underscore picks from req.body the validFields
+	var data = _.pick(req.body, validFields);
+	// Save the new User
 	var newUser = new Model.User(data).save(function (err, newuser) {
 		if (err) {
 			console.log(err);
@@ -21,10 +37,48 @@ exports.create = function(req, res) {
 	});
 };
 exports.show = function(req, res) {
- 
+	 var params = req.params.user;
+	 if (params) {
+	 	Model.User.findOne({
+	 		username: params
+	 	}, function (err, person) {
+			if (err) {
+				console.log(err);
+				res.send(404, err);
+			} else {
+				res.send(201, _.pick(person, showFields));
+			}
+		});
+	 } else {
+	 	res.send(404);
+	 }
 };
 exports.update = function(req, res) {
- 
+	var params = req.params.user;
+	var body = _.pick(req.body, validFields);
+		if (params) {
+			Model.User.findOne({
+		 		username: params
+		 	}, function (err, person) {
+				if (err) {
+					console.log(err);
+					res.send(404, err);
+				} else {
+					var push = _.omit(person, validFields);
+					var newPerson = _.extend(push, body);
+					newPerson.save(function (err){
+						if (err) {
+							console.log(err);
+							res.send(404, err);
+						} else {
+							res.send(201, _.pick(newPerson, showFields));
+						}
+					});
+				}
+			});
+		 } else {
+		 	res.send(404);
+		 }
 };
 exports.destroy = function(req, res) {
  
